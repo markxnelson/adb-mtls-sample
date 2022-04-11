f313a35c9e97a8f2ec092552096d385077fc306e
Mark Nelson <mark.x.nelson@oracle.com>
2022-04-11 10:45:09 -0400

    add a bit of documentation
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

--- src/main/java/com/github/markxnelson/SimpleJDBCTest.java
+++ src/main/java/com/github/markxnelson/SimpleJDBCTest.java
@@ -9,23 +9,36 @@ import oracle.ucp.jdbc.PoolDataSource;
 import oracle.ucp.jdbc.PoolDataSourceFactory;
 
 public class SimpleJDBCTest {
+    // set the database JDBC URL - note that the alias ("myquickstart_high" in this example) and 
+    // the location of the wallet must be changed to match your own environment
     private static String url = "jdbc:oracle:thin:@myquickstart_high?TNS_ADMIN=/home/mark/blog";
+    
+    // the username to connect to the database with
     private static String username = "admin";
 
     public static void main(String[] args) {
 
         try {
+            // read the password from the DB_PASSWORD environment variable
             String password = System.getenv("DB_PASSWORD");
+
+            // create a data source
             PoolDataSource ds = PoolDataSourceFactory.getPoolDataSource();
             ds.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
             ds.setURL(url);
             ds.setUser(username);
+            // when using mutual TLS the password is not required and you can comment out 
+            // or delete the following line
             ds.setPassword(password);
+
+            // try to connect to the database
             System.out.println("Trying to connect...");
             Connection conn = ds.getConnection();
             if (conn != null) {
                 System.out.println("Connected!");
             }
+
+            // clean up
             conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
